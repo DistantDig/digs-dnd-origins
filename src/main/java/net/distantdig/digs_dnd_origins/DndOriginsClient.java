6c496a5e92a7b841c9d1bcafa4879fcac8a3d093
DistantDig <jameswaller711@gmail.com>
2023-04-26 13:21:16 -0700

    Initial commit

---
+++ src/main/java/net/distantdig/digs_dnd_origins/DndOriginsClient.java
@@ -0,0 +1,10 @@
+package net.distantdig.digs_dnd_origins;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DndOriginsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/distantdig/digs_dnd_origins/DndOriginsDataGenerator.java
@@ -0,0 +1,11 @@
+package net.distantdig.digs_dnd_origins;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class DndOriginsDataGenerator implements DataGeneratorEntrypoint {
+    @Override
+    public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+    }
+}
