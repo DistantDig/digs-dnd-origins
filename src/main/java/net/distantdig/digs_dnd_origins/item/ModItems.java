5d0cf0cd429088e33a473a60e207b9919259c30e
DistantDig <jameswaller711@gmail.com>
2023-04-29 15:20:43 -0700

    Added horns item

--- src/main/java/net/distantdig/digs_dnd_origins/item/ModItems.java
+++ src/main/java/net/distantdig/digs_dnd_origins/item/ModItems.java
@@ -10,10 +10,11 @@ import net.minecraft.util.Identifier;
 public class ModItems {
 
     public static final Item RED_SCALE = registerItem("red_scale", new Item((new FabricItemSettings())));
-    public static final Item Blue_SCALE = registerItem("blue_scale", new Item((new FabricItemSettings())));
+    public static final Item BLUE_SCALE = registerItem("blue_scale", new Item((new FabricItemSettings())));
     public static final Item GREEN_SCALE = registerItem("green_scale", new Item((new FabricItemSettings())));
     public static final Item WHITE_SCALE = registerItem("white_scale", new Item((new FabricItemSettings())));
     public static final Item BLACK_SCALE = registerItem("black_scale", new Item((new FabricItemSettings())));
+    public static final Item HORNS = registerItem("horns", new Item((new FabricItemSettings())));
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, new Identifier(DndOrigins.MOD_ID, name), item);
