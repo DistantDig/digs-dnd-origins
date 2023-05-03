6d63c52a8832bcf87c4ae1c0ff640365f88d9ffe
DistantDig <jameswaller711@gmail.com>
2023-05-03 14:06:40 -0700

    created red spit projectile

--- src/main/java/net/distantdig/digs_dnd_origins/DndOrigins.java
+++ src/main/java/net/distantdig/digs_dnd_origins/DndOrigins.java
@@ -2,10 +2,18 @@ package net.distantdig.digs_dnd_origins;
 
 import net.distantdig.digs_dnd_origins.entity.ModEntities;
 import net.distantdig.digs_dnd_origins.entity.custom.ToyFrogEntity;
+import net.distantdig.digs_dnd_origins.entity.projectiles.RedSpitEntity;
 import net.distantdig.digs_dnd_origins.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricDefaultAttributeRegistry;
+import net.fabricmc.fabric.api.object.builder.v1.entity.FabricEntityTypeBuilder;
+import net.minecraft.entity.EntityDimensions;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.SpawnGroup;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -15,6 +23,13 @@ public class DndOrigins implements ModInitializer {
     // That way, it's clear which mod wrote info, warnings, and errors.
     public static final String MOD_ID = "digs_dnd_origins";
     public static final Logger LOGGER = LoggerFactory.getLogger("digs_dnd_origins");
+    public static final EntityType<RedSpitEntity> RedSpitEntityType = Registry.register(
+            Registries.ENTITY_TYPE,
+            new Identifier(MOD_ID, "red_spit"),
+            FabricEntityTypeBuilder.<RedSpitEntity>create(SpawnGroup.MISC, RedSpitEntity::new)
+                    .dimensions(EntityDimensions.fixed(0.25F, 0.25F))
+                    .trackRangeBlocks(4).trackedUpdateRate(10).build()
+    );
     @Override
     public void onInitialize() {
         // This code runs as soon as Minecraft is in a mod-load-ready state.
--- src/main/java/net/distantdig/digs_dnd_origins/DndOriginsClient.java
+++ src/main/java/net/distantdig/digs_dnd_origins/DndOriginsClient.java
@@ -4,10 +4,14 @@ import net.distantdig.digs_dnd_origins.client.ToyFrogRenderer;
 import net.distantdig.digs_dnd_origins.entity.ModEntities;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
+import net.minecraft.client.render.entity.FlyingItemEntityRenderer;
 
 public class DndOriginsClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         EntityRendererRegistry.register(ModEntities.TOY_FROG, ToyFrogRenderer::new);
+        EntityRendererRegistry.register(DndOrigins.RedSpitEntityType, (context) ->
+                new FlyingItemEntityRenderer(context));
     }
+
 }
