649ae73766b55b46cb04d147d58afface331f58f
DistantDig <jameswaller711@gmail.com>
2023-11-19 14:29:49 -0800

    tweaked horns model and texture

--- src/main/java/net/distantdig/digs_dnd_origins/client/HornsArmorModel.java
+++ src/main/java/net/distantdig/digs_dnd_origins/client/HornsArmorModel.java
@@ -8,12 +8,12 @@ import software.bernie.geckolib.model.GeoModel;
 public class HornsArmorModel extends GeoModel<HornsItem> {
     @Override
     public Identifier getModelResource(HornsItem animatable) {
-        return new Identifier(DndOrigins.MOD_ID, "geo/horns_helmet.geo.json");
+        return new Identifier(DndOrigins.MOD_ID, "geo/horns.geo.json");
     }
 
     @Override
     public Identifier getTextureResource(HornsItem animatable) {
-        return new Identifier(DndOrigins.MOD_ID, "textures/armor/horns_helmet.png");
+        return new Identifier(DndOrigins.MOD_ID, "textures/armor/horns.png");
     }
 
     @Override
