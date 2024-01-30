347404565dd4c83faf883f8b8b8380a589cc54cc
DistantDig <jameswaller711@gmail.com>
2024-01-29 18:19:04 -0800

    changed half-elf reputation mixin location

--- src/main/java/net/distantdig/digs_dnd_origins/mixins/VillagerMixin.java
+++ src/main/java/net/distantdig/digs_dnd_origins/mixins/VillagerMixin.java
@@ -1,13 +1,13 @@
 package net.distantdig.digs_dnd_origins.mixins;
 
 import io.github.apace100.apoli.component.PowerHolderComponent;
-import net.distantdig.digs_dnd_origins.DndOrigins;
 import net.distantdig.digs_dnd_origins.power.ReputationPower;
 import net.minecraft.entity.passive.VillagerEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.Optional;
 
@@ -15,16 +15,14 @@ import java.util.Optional;
 public abstract class VillagerMixin {
 
     //increase villager reputation for player having the reputation power
-    @SuppressWarnings("InvalidInjectorMethodSignature")
-    @ModifyVariable(method = "prepareOffersFor", at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/entity/passive/VillagerEntity;getReputation(Lnet/minecraft/entity/player/PlayerEntity;)I"))
-    private int increaseReputation(int i, PlayerEntity player) {
+    @Inject(method = "getReputation", at = @At(value = "RETURN"), cancellable = true)
+    private void increaseReputation(PlayerEntity player, CallbackInfoReturnable<Integer> cir) {
         if (PowerHolderComponent.getPowers(player, ReputationPower.class).stream().anyMatch(ReputationPower::shouldApply)) {
 
-            Optional<Integer> repModifier = PowerHolderComponent.KEY.get(player).getPowers(ReputationPower.class).stream().filter(ReputationPower::isActive).map(ReputationPower::getReputationModifier).max(Integer::compareTo);
-            int finalI = i;
-            i = repModifier.map(aInt -> Math.addExact(aInt, finalI)).orElse(i);
+            Optional<Integer> repModifier = PowerHolderComponent.KEY.get(player).getPowers(ReputationPower.class)
+                    .stream().filter(ReputationPower::isActive).map(ReputationPower::getReputationModifier).max(Integer::compareTo);
+
+            cir.setReturnValue(repModifier.map(aInt -> Math.addExact(aInt, cir.getReturnValue())).orElse(cir.getReturnValue()));
         }
-        DndOrigins.LOGGER.info(String.valueOf(i));
-        return i;
     }
 }
